20a2295dbe96c99c15de16ee8dd5b27da706264a
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-05-31 16:28:03 -0600

    Implement Lantern model and component structure

---
+++ src/app/lanterns/lantern.model.ts
@@ -0,0 +1,9 @@
+export class Lantern {
+  constructor(
+    public id: string,
+    public model: string,
+    public type: string,
+    public imageUrl: string,
+    public datasheetUrl: string
+  ) {}
+}
