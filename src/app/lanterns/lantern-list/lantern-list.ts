a1568c6590a2bb851d42a77b55082ef6aad8f034
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-06 12:09:31 -0600

    Implement lantern selection EventEmitter and hover effects

--- src/app/lanterns/lantern-list/lantern-list.ts
+++ src/app/lanterns/lantern-list/lantern-list.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Lantern } from '../lantern.model';
 import { LanternService } from '../lantern.service';
 
@@ -10,10 +10,15 @@ import { LanternService } from '../lantern.service';
 })
 export class LanternList implements OnInit {
   lanterns: Lantern[] = [];
+  @Output() lanternSelected = new EventEmitter<Lantern>();
 
   constructor(private lanternService: LanternService) {}
 
   ngOnInit(): void {
     this.lanterns = this.lanternService.getLanterns();
+  }
+
+  onSelectedLantern(lantern: Lantern): void {
+    this.lanternSelected.emit(lantern);
   }
 }
