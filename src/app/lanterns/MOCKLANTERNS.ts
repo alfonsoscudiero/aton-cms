5b3a8354e12cb2d5a85d2f2bbced70c0c370a0d8
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-06 11:04:13 -0600

    Add MOCKLANTERNS data and separate from LanternService

---
+++ src/app/lanterns/MOCKLANTERNS.ts
@@ -0,0 +1,60 @@
+import { Lantern } from './lantern.model';
+
+export const MOCKLANTERNS: Lantern[] = [
+  {
+    id: '1',
+    model: 'NS35',
+    type: 'LED Marine Lantern',
+    imageUrl: 'assets/images/lanterns/lantern-ns35.png',
+    datasheetUrl: 'https://www.automaticpower.com/images/datasheets/marine_lanterns/PMAPI-NS35_Pier_Light_LED_Marine_Lantern.pdf'
+  },
+  {
+    id: '2',
+    model: 'FA249',
+    type: 'LED Marine Lantern',
+    imageUrl: 'assets/images/lanterns/lantern-fa249.png',
+    datasheetUrl: 'https://www.automaticpower.com/images/datasheets/marine_lanterns/FA249_LED_RF14_Marine_Lantern_R5.pdf'
+  },
+  {
+    id: '3',
+    model: 'FA250 Long Range',
+    type: 'LED Marine Lantern',
+    imageUrl: 'assets/images/lanterns/lantern-fa250-lr.png',
+    datasheetUrl: 'https://www.automaticpower.com/images/datasheets/marine_lanterns/FA-250_Long_Range_LED_Marine_Lantern.pdf'
+  },
+  {
+    id: '4',
+    model: 'FA250 Medium Range',
+    type: 'LED Marine Lantern',
+    imageUrl: 'assets/images/lanterns/lantern-fa250-mr.png',
+    datasheetUrl: 'https://www.automaticpower.com/images/datasheets/marine_lanterns/FA-250_Medium_Range_LED_Marine_Lantern.pdf'
+  },
+  {
+    id: '5',
+    model: 'SC12',
+    type: 'Self Contained LED Marine Lantern',
+    imageUrl: 'assets/images/lanterns/lantern-sc12.png',
+    datasheetUrl: 'https://www.automaticpower.com/images/datasheets/marine_lanterns/PMAPI-SC12_Self_Contained_LED_Marine_Lantern_R1.pdf'
+  },
+  {
+    id: '6',
+    model: 'SC35',
+    type: 'Self Contained LED Marine Lantern',
+    imageUrl: 'assets/images/lanterns/lantern-sc35.png',
+    datasheetUrl: 'https://www.automaticpower.com/images/datasheets/marine_lanterns/PMAPI-SC35_Self_Contained_LED_Marine_Lantern.pdf'
+  },
+  {
+    id: '7',
+    model: 'SC36',
+    type: 'Self Contained LED Marine Lantern',
+    imageUrl: 'assets/images/lanterns/lantern-sc36.png',
+    datasheetUrl: 'https://www.automaticpower.com/images/datasheets/marine_lanterns/PMAPI-SC36_Self_Contained_LED_Marine_Lantern_with_AIS.pdf'
+  },
+  {
+    id: '8',
+    model: 'SC37',
+    type: 'Self Contained LED Marine Lantern',
+    imageUrl: 'assets/images/lanterns/lantern-sc37.png',
+    datasheetUrl: 'https://www.automaticpower.com/images/datasheets/marine_lanterns/PMAPI-SC37_Self_Contained_LED_Marine_Lantern_with_AIS.pdf'
+  }
+];
--- src/app/lanterns/lantern.service.ts
+++ src/app/lanterns/lantern.service.ts
@@ -1,26 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Lantern } from './lantern.model';
+import { MOCKLANTERNS } from './MOCKLANTERNS';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanternService {
-  private lanterns: Lantern[] = [
-    new Lantern(
-      '1',
-      'PMAPI-SC35',
-      'Self-Contained LED',
-      'assets/images/lanterns/sc35.webp',
-      'assets/datasheets/PMAPI-SC35_Self_Contained_LED_Marine_Lantern.pdf'
-    ),
-    new Lantern(
-      '2',
-      'PMAPI-M850',
-      'LED Marine Lantern',
-      'assets/images/lanterns/m850.webp',
-      'https://example.com/m850.pdf'
-    )
-  ];
+  private lanterns: Lantern[] = MOCKLANTERNS;
 
   getLanterns(): Lantern[] {
     return this.lanterns.slice();
