037b6b84a5614c6bb989a557a6715998ddf6a1dc
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-06 19:29:09 -0600

    Add lanternChangedEvent and emit updates after delete

--- src/app/lanterns/lantern.service.ts
+++ src/app/lanterns/lantern.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Lantern } from './lantern.model';
 import { MOCKLANTERNS } from './MOCKLANTERNS';
 
@@ -6,6 +6,8 @@ import { MOCKLANTERNS } from './MOCKLANTERNS';
   providedIn: 'root'
 })
 export class LanternService {
+  lanternChangedEvent = new EventEmitter<Lantern[]>();
+
   private lanterns: Lantern[] = MOCKLANTERNS;
 
   getLanterns(): Lantern[] {
@@ -13,10 +15,17 @@ export class LanternService {
   }
 
   deleteLantern(lantern: Lantern): void {
+    if (!lantern) {
+      return;
+    }
+
     const index = this.lanterns.indexOf(lantern);
 
-    if (index >= 0) {
-      this.lanterns.splice(index, 1);
+    if (index < 0) {
+      return;
     }
+
+    this.lanterns.splice(index, 1);
+    this.lanternChangedEvent.emit(this.lanterns.slice());
   }
 }
