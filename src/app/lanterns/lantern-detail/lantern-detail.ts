1ce5d15a2460cd2be624efd10bf731774022b04b
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-06 19:05:05 -0600

    Add Delete Lantern event emitter to detail component

--- src/app/lanterns/lantern-detail/lantern-detail.ts
+++ src/app/lanterns/lantern-detail/lantern-detail.ts
@@ -12,9 +12,18 @@ export class LanternDetail {
   @Input() lantern?: Lantern;
 
   @Output() editLanternSelected = new EventEmitter<void>();
+  @Output() deleteLanternSelected = new EventEmitter<Lantern>();
 
   onEditLantern(): void {
     this.editLanternSelected.emit();
+  }
+
+  onDeleteLantern(): void {
+    if (!this.lantern) {
+      return;
+    }
+
+    this.deleteLanternSelected.emit(this.lantern);
   }
 
 }
