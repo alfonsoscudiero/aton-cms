8ffea8b967a0606b67c7c3faae8fff7091b32b44
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-07 15:45:01 -0600

    Add BuoyItem component, styling, and router navigation

--- src/app/buoys/buoy-item/buoy-item.ts
+++ src/app/buoys/buoy-item/buoy-item.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Buoy } from '../buoy.model';
 
 @Component({
   selector: 'aton-buoy-item',
   standalone: false,
   templateUrl: './buoy-item.html',
-  styleUrl: './buoy-item.css',
+  styleUrl: './buoy-item.css'
 })
-export class BuoyItem {}
+export class BuoyItem {
+
+  @Input() buoy!: Buoy;
+
+}
