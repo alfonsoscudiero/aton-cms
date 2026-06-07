74be425d1ad2a18149e4482be7d477e73d043145
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-07 15:54:48 -0600

    Add child routing for Buoys detail and edit components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { Lanterns } from './lanterns/lanterns';
 import { Buoys } from './buoys/buoys';
+import { BuoyDetail } from './buoys/buoy-detail/buoy-detail';
+import { BuoyEdit } from './buoys/buoy-edit/buoy-edit';
 
 const routes: Routes = [
   { path: '', redirectTo: '/lanterns', pathMatch: 'full' },
   { path: 'lanterns', component: Lanterns },
-  { path: 'buoys', component: Buoys },
+  { path: 'buoys', component: Buoys,
+    children: [
+      { path: 'new', component: BuoyEdit },
+      { path: ':id', component: BuoyDetail },
+      { path: ':id/edit', component: BuoyEdit }
+    ]
+  },
 ];
 
 @NgModule({
