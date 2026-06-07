207a9189c96bbbc4170c2f2a712683ac30004a17
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-07 15:19:25 -0600

    Add BuoyService methods and Subject for buoy updates

--- src/app/buoys/buoy.service.ts
+++ src/app/buoys/buoy.service.ts
@@ -1,6 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+import { Buoy } from './buoy.model';
+import { MOCKBUOYS } from './MOCKBUOYS';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Buoy {}
+export class BuoyService {
+  buoysChangedEvent = new Subject<Buoy[]>();
+
+  private buoys: Buoy[] = MOCKBUOYS.slice();
+
+  getBuoys(): Buoy[] {
+    return this.buoys.slice();
+  }
+
+  getBuoy(id: string): Buoy | undefined {
+    return this.buoys.find((buoy) => buoy.id === id);
+  }
+
+  deleteBuoy(buoy: Buoy): void {
+    const index = this.buoys.indexOf(buoy);
+
+    if (index !== -1) {
+      this.buoys.splice(index, 1);
+      this.buoysChangedEvent.next(this.buoys.slice());
+    }
+  }
+}
