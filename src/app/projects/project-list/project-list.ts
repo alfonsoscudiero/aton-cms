7cac02fbfa6b51d065d6db971a931aa262f98ecf
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-14 12:19:34 -0600

    Connect ProjectList to ProjectService and load project data

--- src/app/projects/project-list/project-list.ts
+++ src/app/projects/project-list/project-list.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Project } from '../project.model';
+import { ProjectService } from '../project.service';
 
 @Component({
   selector: 'aton-project-list',
@@ -6,4 +9,17 @@ import { Component } from '@angular/core';
   templateUrl: './project-list.html',
   styleUrl: './project-list.css',
 })
-export class ProjectList {}
+
+export class ProjectList implements OnInit {
+  projects: Project[] = [];
+
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit(): void {
+    this.projects = this.projectService.getProjects();
+
+    this.projectService.projectChangedEvent.subscribe((projects: Project[]) => {
+      this.projects = projects;
+    });
+  }
+}
