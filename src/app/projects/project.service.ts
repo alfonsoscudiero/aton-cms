b7ffed2f6bc83401ce85620d98d2d2690fc9ebc6
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-23 20:44:07 -0600

    Add addProject and updateProject methods to ProjectService

--- src/app/projects/project.service.ts
+++ src/app/projects/project.service.ts
@@ -36,4 +36,34 @@ export class ProjectService {
     this.projects.splice(index, 1);
     this.projectChangedEvent.next(this.projects.slice());
   }
+
+  addProject(project: Project): void {
+    if (!project) {
+      return;
+    }
+
+    const maxId = this.projects.reduce((max, currentProject) => {
+      const currentId = Number(currentProject.id);
+      return currentId > max ? currentId : max;
+    }, 0);
+
+    project.id = String(maxId + 1);
+
+    this.projects.push(project);
+    this.projectChangedEvent.next(this.projects.slice());
+  }
+
+  updateProject(originalProject: Project, newProject: Project): void {
+    if (!originalProject || !newProject) {
+      return;
+    }
+
+    const index = this.projects.indexOf(originalProject);
+
+    if (index !== -1) {
+      newProject.id = originalProject.id;
+      this.projects[index] = newProject;
+      this.projectChangedEvent.next(this.projects.slice());
+    }
+  }
 }
